1bf5a61ba5a4d573afa31821d3fc8dc46180cd56
AdrianPokos <adrian.pokos@gmail.com>
2025-10-03 18:50:16 +1000

    venv set up + connection of db

--- SQLi/main.py
+++ SQLi/main.py
@@ -1,1 +1,40 @@
-# IMPORT DATABASE
+import psycopg2
+
+#Connect to database
+connection = psycopg2.connect(
+    host="localhost",
+    database="sqli_db",
+    user="postgres",
+    password="250705",
+)
+connection.set_session(autocommit=True)
+
+#Cursor
+
+#with connection.cursor() as cursor:
+#    cursor.execute('SELECT COUNT(*) FROM users')
+#    result =  cursor.fetchone()
+#print(result)
+
+#Cursor
+def is_admin(username: str) -> bool:
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT
+                admin
+            FROM
+                users
+            WHERE
+                username = '%s'
+        """ % username)
+        result = cursor.fetchone()
+
+    if result is None:
+        # User does not exist
+        return False
+
+    admin, = result
+    return admin
+
+#For testing
+print(is_admin(input('')))
